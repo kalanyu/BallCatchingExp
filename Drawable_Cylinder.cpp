86de6ea7268c14f145043a15f92ce1d7114bb66b
kalanyu <kalanyuz@Kalanyus-MacBook-Pro.local>
2015-12-16 14:03:08 +0900

    Start cylinder branch

---
+++ Drawable_Cylinder.cpp
@@ -0,0 +1,72 @@
+//
+//  Drawable_Ball.cpp
+//
+//  Created by Hiroyuki KAMBARA on 12/02/29.
+//  Copyright 2012年 Tokyo Institute of Techonology. All rights reserved.
+//
+
+#include "Drawable_Cylinder.h"
+
+const int Drawable_Cylinder::Nslices = 30;
+const int Drawable_Cylinder::Nstacks = 20;
+
+const double default_radius = 0.05;
+
+
+Drawable_Cylinder::Drawable_Cylinder(float newr, float newg, float newb, vector<float> newpos)
+:DrawableObject(newr,newg,newb,newpos)
+{
+    radius = default_radius;
+    quadric = gluNewQuadric();
+    gluQuadricDrawStyle(quadric, GLU_FILL);
+    gluQuadricOrientation(quadric, GLU_INSIDE);
+}
+
+
+Drawable_Cylinder::Drawable_Cylinder(const float newcolor[], vector<float> newpos)
+:DrawableObject(newcolor,newpos)
+{
+    radius = default_radius;
+    quadric = gluNewQuadric();
+    gluQuadricDrawStyle(quadric, GLU_FILL);
+    gluQuadricOrientation(quadric, GLU_INSIDE);
+}
+
+
+Drawable_Cylinder::Drawable_Cylinder()
+:DrawableObject()
+{
+    radius = default_radius;
+    quadric = gluNewQuadric();
+    gluQuadricDrawStyle(quadric, GLU_FILL);
+    gluQuadricOrientation(quadric, GLU_INSIDE);
+}
+
+
+void Drawable_Cylinder::draw()
+{
+    glPushMatrix();
+
+    glTranslated(pos[0], pos[1], pos[2]);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color);
+    gluCylinder(quadric, 0.1f, 0.1f, 10, 32, 32);
+
+    glPopMatrix();
+}
+
+
+void Drawable_Cylinder::setRadius(double newradius)
+{
+    radius = newradius;
+}
+
+
+double Drawable_Cylinder::getRadius()
+{
+    return radius;
+}
+
+Drawable_Cylinder::~Drawable_Cylinder()
+{
+  gluDeleteQuadric(quadric);
+}
---
+++ Drawable_Cylinder.h
@@ -0,0 +1,35 @@
+//
+//  Drawable_Ball.h
+//
+//  Created by Kalanyu ZINTUS-ART on 15/12/16.
+//  Copyright 2015年 Tokyo Institute of Techonology. All rights reserved.
+//
+
+#ifndef Drawable_Ball_h
+#define Drawable_Ball_h
+
+#include "DrawableObject.h"
+
+class Drawable_Ball : public DrawableObject {
+
+private:
+  double radius;
+  double height;
+  static const int Nslices;
+  static const int Nstacks;
+  GLUquadric *quadric;
+
+public:
+  Drawable_Cylinder(float newr, float newg, float newb, vector<float> newpos);
+  Drawable_Cylinder(const float newcolor[], vector<float> newpos);
+  Drawable_Cylinder();
+  void draw();
+  void setRadius(double newradius);
+  void setHeight(double newheight);
+  double getRadius();
+  double getHeight();
+};
+
+
+
+#endif
